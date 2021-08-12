efa18f348508eae87abf362996ff9d6ffe242ca6
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-08-12 13:19:28 +0300

    Лишняя функция

--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -7,10 +7,3 @@ export const dataAdapter = (...data) => {
   const  dataFormat = data.reduce((obj, current) => Object.assign(obj,current), {});
   return dataFormat;
 };
-
-export const createElement = (template) => {
-  const newElement = document.createElement('div'); // 1
-  newElement.innerHTML = template; // 2
-
-  return newElement.firstChild; // 3
-};
