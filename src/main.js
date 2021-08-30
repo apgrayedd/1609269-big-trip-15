85fd64d1a1ef520f45ca59bc16ceb0309f369480
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-08-30 10:34:01 +0300

    Удалил комментарий

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import {
   dataPoints
 } from './mock/data.js';
 
-// const data = dataAdapter(pointArr, destination());
 const data = dataAdapter(dataPoints);
 const tripEvents = document.querySelector('.trip-events');
 const presenterTrip = new TripPresenter(tripEvents);
